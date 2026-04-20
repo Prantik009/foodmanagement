c85e1362c0db5863bf90f74821940191525a10a9
Prantik Biswas <103238660+Prantik009@users.noreply.github.com>
2026-04-20 22:49:07 +0530

    Modify MongoDB connection URL
    Updated MongoDB connection URL to remove retryWrites parameter.

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-  const URL = `mongodb+srv://workingad512:ZexI40kUhXfFSc2p@cluster0.46wki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  // const URL = `mongodb+srv://workingad512:ZexI40kUhXfFSc2p@cluster0.46wki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const URL = `mongodb+srv://workingad512:ZexI40kUhXfFSc2p@cluster0.46wki.mongodb.net/?appName=Cluster0`;
   try {
     await mongoose.connect(URL);
     console.log("DB Connected");
